622d9c1de9e3bde0f598a7157909c8083da5ae87
Anirudha Jadhav <anirudhasj441@gmail.com>
2024-10-11 23:50:34 +0530

    dev: update loading backdrop

--- pr6-cloud-ide/src/components/Loading.tsx
+++ pr6-cloud-ide/src/components/Loading.tsx
@@ -1,4 +1,4 @@
-import { Backdrop, CircularProgress } from "@mui/material";
+import { Backdrop, Typography } from "@mui/material";
 import React from "react";
 
 const Loading: React.FC<{ open?: boolean }> = (props: { open?: boolean }) => {
@@ -6,9 +6,47 @@ const Loading: React.FC<{ open?: boolean }> = (props: { open?: boolean }) => {
         <>
             <Backdrop
                 open={props.open ?? true}
-                sx={(theme) => ({ zIndex: theme.zIndex.drawer + 1 })}
+                sx={(theme) => ({
+                    zIndex: theme.zIndex.drawer + 1,
+                    background: theme.palette.background.default,
+                })}
             >
-                <CircularProgress />
+                <div>
+                    <div className="text-start relative">
+                        <Typography
+                            variant="h4"
+                            color={"red"}
+                            fontWeight={"bolder"}
+                            sx={{
+                                transform: "translateX(-50%)",
+                                width: "max-content",
+                            }}
+                        >
+                            {"<Devil>"}
+                        </Typography>
+                    </div>
+                    <div className="text-center relative">
+                        <Typography variant="h4" fontWeight={"bolder"}>
+                            LOADING...
+                        </Typography>
+                    </div>
+                    <div className="text-end relative bg-red-500">
+                        <Typography
+                            variant="h4"
+                            fontWeight={"bolder"}
+                            color={"red"}
+                            sx={{
+                                position: "absolute",
+                                right: "0",
+                                top: "0",
+                                transform: "translateX(50%)",
+                                width: "max-content",
+                            }}
+                        >
+                            {"</Devil>"}
+                        </Typography>
+                    </div>
+                </div>
             </Backdrop>
         </>
     );
